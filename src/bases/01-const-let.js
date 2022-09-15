ce4b924eb8dbbf06515deb0f4dd3399c767bf7e6
Eugenia Chacon <eugenia.chacon@autodesk.com>
2022-09-15 10:23:05 -0300

    initial commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// variables y constantes
+
+const nombre = 'Eugenia';
+const apellido = 'Chacon';
+
+let valorDado = 5;
+valorDado = 4;
+console.log( nombre, apellido, valorDado);
+
+
+if ( true ) {
+  let valorDado = 6;
+  const nombre = 'Peter';
+  console.log(valorDado);
+  console.log(nombre);
+}
+
+console.log(valorDado);
---
+++ src/bases/02-template-string.js
@@ -0,0 +1,13 @@
+const nombre = 'Eugenia';
+const apellido = 'Chacon';
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+function getSaludo(nombre) {
+  return 'Hola ' + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)} `);
